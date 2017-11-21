2e7baef61216613c731e5a68620013a623d1f9fa
Alisson Gomes <alisson.php@gmail.com>
2017-11-21 00:20:12 -0300

    categories crud

--- app/Modules/Items/Controllers/CategoryController.php
+++ app/Modules/Items/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Modules\Items\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Modules\Items\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
@@ -17,11 +18,35 @@ class CategoryController extends Controller
 
     public function index()
     {
-
+        try {
+            $cats = $this->model->all();
+            return response($cats,200);
+        } catch (\Exception $ex){
+            return response($ex->getMessage(), 500);
+        }
     }
-    public function store() {}
+    public function store(Request $request)
+    {
+        try {
+            $new = $this->model->create([
+                'label' => $request->input('label')
+            ]);
+
+            return response($new, 200);
+        } catch (\Exception $ex) {
+            return response($ex->getMessage(),500);
+        }
+    }
     public function show() {}
     public function update() {}
-    public function delete() {}
+    public function delete($id)
+    {
+        try {
+            $this->model->find($id)->delete();
+            return response('deleted',200);
+        } catch (\Exception $ex) {
+            return response($ex->getMessage(),500);
+        }
+    }
 
 }
--- app/Modules/Items/Models/Category.php
+++ app/Modules/Items/Models/Category.php
@@ -8,6 +8,8 @@ class Category extends Model
 {
 
     protected $table = 'categories';
+    protected $fillable = ['label'];
+    public $timestamps = false;
 
     public function subcategories()
     {
